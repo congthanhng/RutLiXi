2d0a5fa1f86a77c2a9d6b54d28542bcfb26517f6
congthanhng <thanhcong.uit@gmail.com>
2020-02-19 23:05:46 +0700

    add userEntity

---
+++ app/src/main/java/vn/tcong/rutlixi/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package vn.tcong.rutlixi.entity;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class UserEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "birthDay")
+    private int birthDay;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(int birthDay) {
+        this.birthDay = birthDay;
+    }
+}
